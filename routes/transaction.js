d250b4df44daade469f903129b6f8c812a6c5717
Ana Ceprnja <aceprnja@iun.edu>
2021-03-09 17:50:51 -0700

    working on router

---
+++ routes/transaction.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const ctrl = require('../controllers');
+
+router.get('/')
+router.get(getTransactions);
+router.post(addTransactions);
+
+
+
+
+module.exports = router;
